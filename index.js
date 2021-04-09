5aa8e75a9c174ce6666046de9bdeea9f13fbd53b
Usman Akram <usman.akram@gmail.com>
2021-04-09 16:00:30 +0500

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.get("/", function (req, res) {
   res.send("Hello Class A Section");
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log("Listening on port " + port);
 });
